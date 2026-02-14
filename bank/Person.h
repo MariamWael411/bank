072fe67e863c0666f9f1b08dc28bef68165e1077
MariamWael411 <dfgsadffg344@gmail.com>
2026-02-14 13:09:42 +0200

    Client

--- bank/Person.h
+++ bank/Person.h
@@ -11,16 +11,52 @@ protected:
     string password;
 
 public:
-    Person();
-    Person(int id, string name, string password);
+    Person() {
+        id = 0;
+        name = "";
+        password = "";
+    }
 
-    void setId(int id);
-    void setName(string name);
-    void setPassword(string password);
+    Person(int id, string name, string password) {
+        this->id = id;
+        this->name = name;
+        this->password = password;
+    }
 
-    int getId() const;
-    string getName() const;
-    string getPassword() const;
+    void setId(int id) {
+        this->id = id;
+    }
 
-    virtual void display() const;
+    void setName(string name) {
+        if (Validation::isValidName(name))
+            this->name = name;
+        else
+            cout << "Name must be Alphabetic and at least size 3 and max 20" << endl;
+    }
+
+    void setPassword(string password) {
+        this->password = Validation::ValidatePassword(password);
+            
+    }
+
+    int getId()  {
+        return id;
+    }
+
+    string getName()  {
+        return name;
+    }
+
+    string getPassword()  {
+        return password;
+    }
+
+    void display()  {
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "Password: " << password << endl;
+    }
 };
+
+
+
